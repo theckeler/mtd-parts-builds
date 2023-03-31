1034514f4192e24baa39bbbaa2a7f5defe0cabfb
Todd Heckeler <todd@consumedesign.com>
2023-03-31 07:45:44 -0400

    updates

--- src/components/elements/SupportModel.js
+++ src/components/elements/SupportModel.js
@@ -1,26 +1,10 @@
+import Button from "../elements/Button";
+
 const SupportModel = ({ title, sub, addClass, addStyle }) => {
 	return (
-		<form
-			action="/on/demandware.store/Sites-troybilt-Site/en_US/Search-Show"
-			className="flex flex-column h-100"
-			method="get"
-			name="simpleSearch"
-			noValidate=""
-			role="search">
+		<div className="flex flex-column h-100">
 			<h2 className="text-center">{title}</h2>
 			<p className="text-center">{sub}</p>
-
-			<label htmlFor="search-by-number">Model #, Part # or Item #</label>
-
-			<input
-				autoComplete="off"
-				className="form-control search-field"
-				id="search-by-number"
-				name="q"
-				required=""
-				type="search"
-				aria-describedby="16680858695420"
-			/>
 
 			<div className="mt-3">
 				<img
@@ -35,10 +19,12 @@ const SupportModel = ({ title, sub, addClass, addStyle }) => {
 				</p>
 			</div>
 
-			<button className="btn btn-secondary btn-pf-search mt-auto" type="submit">
-				Find Support
-			</button>
-		</form>
+			<Button
+				copy="Find Support"
+				url="https://www.mtdparts.com/en_US/ari-partstream.html"
+				addClass="btn btn-block btn-secondary mt-auto"
+			/>
+		</div>
 	);
 };
 
