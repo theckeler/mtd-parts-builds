653174d29a54063e5b295dc6edd012fdc004a8bc
Todd Heckeler <todd@consumedesign.com>
2023-12-15 10:27:46 -0500

    cust ser

--- app/customer-service/page.js
+++ app/customer-service/page.js
@@ -5,7 +5,6 @@ import IconAwards from "@/components/icons/IconAwards";
 import IconBelt from "@/components/icons/IconBelt";
 import IconBlades from "@/components/icons/IconBlades";
 import IconChat from "@/components/icons/IconChat";
-import IconLevel from "@/components/icons/IconLevel";
 import IconMagGlassBolt from "@/components/icons/IconMagGlassBolt";
 import IconManuals from "@/components/icons/IconManuals";
 import IconPhone from "@/components/icons/IconPhone";
@@ -16,17 +15,60 @@ const howToContent = {
     {
       icon: <IconBelt className="w-full max-h-[80px] h-full" />,
       title: "How to Change the Deck Belt on a Riding Mower",
-      href: "https://www.troybilt.com/en_US/knowledge-tb-how-to-change-the-deck-belt-on-a-troy-bilt-riding-mower.html",
+      href: "https://www.mtdparts.com/en_US/knowledge-change-rider-deck-belt-repairs.html",
     },
     {
       icon: <IconBlades className="w-full h-full" />,
       title: "How to Change the Blades on a Riding Mower",
-      href: "https://www.troybilt.com/en_US/knowledge-tb-how-to-change-the-blade-on-your-troy-bilt-rider.html",
+      href: "https://www.mtdparts.com/en_US/knowledge-mower-blade-replacement-maintenance.html",
     },
     {
-      icon: <IconLevel className="w-full max-h-[50px] h-full" />,
-      title: "How to Level the Deck on a Riding Mower",
-      href: "https://www.troybilt.com/en_US/How+Do+I+Level+the+Deck+on+My+Riding+Mower%3F.html",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-full h-full"
+          viewBox="0 0 115.1 127.4"
+        >
+          <path
+            d="M33.8 77h79.4v7.6H33.8V77z"
+            class="st0"
+          />
+          <path d="M113.2 86.5H33.8c-1 0-1.8-.8-1.8-1.8V77c0-1 .8-1.8 1.8-1.8h79.4c1 0 1.8.8 1.8 1.8v7.6c.1 1-.8 1.9-1.8 1.9zm-77.6-3.7h75.7v-3.9H35.6v3.9z" />
+          <path
+            d="M33.8 26.7h79.4v7.6H33.8v-7.6z"
+            class="st0"
+          />
+          <path d="M113.2 36.1H33.8c-1 0-1.8-.8-1.8-1.8v-7.6c0-1 .8-1.8 1.8-1.8h79.4c1 0 1.8.8 1.8 1.8v7.6c.1 1-.8 1.8-1.8 1.8zm-77.6-3.7h75.7v-3.9H35.6v3.9z" />
+          <path d="M35.7 77c-.5 0-.9-.4-.9-.9V34.5c0-.5.4-.9.9-.9s.9.4.9.9v41.6c0 .5-.4.9-.9.9zm5 0c-.5 0-.9-.4-.9-.9V34.5c0-.5.4-.9.9-.9s.9.4.9.9v41.6c0 .5-.4.9-.9.9zm5.1 0c-.5 0-.9-.4-.9-.9V34.5c0-.5.4-.9.9-.9s.9.4.9.9v41.6c-.1.5-.5.9-.9.9zm5 0c-.5 0-.9-.4-.9-.9V34.5c0-.5.4-.9.9-.9s.9.4.9.9v41.6c0 .5-.4.9-.9.9zm5 0c-.5 0-.9-.4-.9-.9V34.5c0-.5.4-.9.9-.9s.9.4.9.9v41.6c0 .5-.4.9-.9.9zm5.1 0c-.5 0-.9-.4-.9-.9V34.5c0-.5.4-.9.9-.9s.9.4.9.9v41.6c-.1.5-.5.9-.9.9zm5 0c-.5 0-.9-.4-.9-.9V34.5c0-.5.4-.9.9-.9s.9.4.9.9v41.6c0 .5-.4.9-.9.9zm5 0c-.5 0-.9-.4-.9-.9V34.5c0-.5.4-.9.9-.9s.9.4.9.9v41.6c0 .5-.4.9-.9.9zm5.1 0c-.5 0-.9-.4-.9-.9V34.5c0-.5.4-.9.9-.9s.9.4.9.9v41.6c-.1.5-.4.9-.9.9zm5 0c-.5 0-.9-.4-.9-.9V34.5c0-.5.4-.9.9-.9s.9.4.9.9v41.6c0 .5-.4.9-.9.9zm5.1 0c-.5 0-.9-.4-.9-.9V34.5c0-.5.4-.9.9-.9s.9.4.9.9v41.6c-.1.5-.5.9-.9.9zm5 0c-.5 0-.9-.4-.9-.9V34.5c0-.5.4-.9.9-.9s.9.4.9.9v41.6c0 .5-.4.9-.9.9zm5 0c-.5 0-.9-.4-.9-.9V34.5c0-.5.4-.9.9-.9s.9.4.9.9v41.6c0 .5-.4.9-.9.9zm5.1 0c-.5 0-.9-.4-.9-.9V34.5c0-.5.4-.9.9-.9s.9.4.9.9v41.6c-.1.5-.5.9-.9.9zm5 0c-.5 0-.9-.4-.9-.9V34.5c0-.5.4-.9.9-.9s.9.4.9.9v41.6c0 .5-.4.9-.9.9zm5 0c-.5 0-.9-.4-.9-.9V34.5c0-.5.4-.9.9-.9s.9.4.9.9v41.6c0 .5-.4.9-.9.9z" />
+          <path
+            d="M55.4 19.3V6.5H39.9v12.8L6.6 43.2c-3 2.3-4.8 5.8-4.8 9.6v67.1c0 3.1 2.5 5.6 5.6 5.6H60c3.1 0 5.6-2.5 5.6-5.6V50.6c0-2.5-.4-5-1.3-7.3l-8.9-24z"
+            class="st0"
+          />
+          <path
+            fill="#08663a"
+            d="M.9 72v47.5c0 3 2.1 5.5 4.7 5.5h55c2.6 0 4.7-2.5 4.7-5.5V72H.9z"
+          />
+          <path
+            fill="#20b466"
+            d="M.9 53.3h64.4V72H.9V53.3z"
+          />
+          <path d="M60 127.4H7.5c-4.1 0-7.5-3.4-7.5-7.5V52.8c0-4.3 2.1-8.5 5.5-11.1L38 18.3V6.5c0-1 .8-1.8 1.8-1.8h15.5c1 0 1.8.8 1.8 1.8v12.4L66 42.6c1 2.6 1.4 5.3 1.4 8v69.3c.1 4.1-3.2 7.5-7.4 7.5zM41.7 8.3v10.9c0 .6-.3 1.1-.8 1.5l-33.2 24c-2.5 1.9-4 4.9-4 8.1v67.1c0 2.1 1.7 3.8 3.8 3.8H60c2.1 0 3.8-1.7 3.8-3.8V50.6c0-2.3-.4-4.6-1.2-6.7l-9-24c-.1-.2-.1-.4-.1-.6v-11H41.7z" />
+          <path d="M54.4 50.3H13.1c-.5 0-.9-.4-.9-.9s.4-.9.9-.9h41.4c.5 0 .9.4.9.9s-.5.9-1 .9zm0 64.9H13.1c-.5 0-.9-.4-.9-.9V60.4c0-.5.4-.9.9-.9h41.4c.5 0 .9.4.9.9v53.9c-.1.5-.5.9-1 .9zm-40.5-1.8h39.6V61.3H13.9v52.1z" />
+          <path
+            d="M56.3.9H38.9c-.6 0-1.1.5-1.1 1.1v9.5h19.7V2c0-.6-.5-1.1-1.2-1.1"
+            class="st0"
+          />
+          <path d="M57.5 12.4H37.8c-.5 0-.9-.4-.9-.9V2c0-1.1.9-2 2-2h17.4c1.1 0 2 .9 2 2v9.5c.1.5-.3.9-.8.9zm-18.9-1.8h18V2c0-.2-.1-.3-.3-.3H38.9c-.2 0-.3.1-.3.3v8.6z" />
+          <path
+            d="M37.8 11.3h19.7v3.4H37.8v-3.4z"
+            class="st0"
+          />
+          <path d="M57.5 15.5H37.8c-.5 0-.9-.4-.9-.9v-3.4c0-.5.4-.9.9-.9h19.7c.5 0 .9.4.9.9v3.4c0 .5-.4.9-.9.9zm-18.9-1.7h18v-1.7h-18v1.7z" />
+        </svg>
+      ),
+
+      title: "How to Change the Oil on a Riding Mower",
+      href: "https://www.mtdparts.com/en_US/knowledge-riding-mower-oil-change-maintenance.html",
     },
   ],
   button: {
@@ -42,7 +84,7 @@ const popularHelpLinks = {
       copy: "Search parts for your machine or view diagrams",
       button: [
         {
-          href: "https://www.troybilt.com/en_US/service-and-parts",
+          href: "https://www.mtdparts.com/en_US/ari-partstream.html",
           copy: "Find parts",
         },
       ],
@@ -52,7 +94,7 @@ const popularHelpLinks = {
       copy: "Use your model number to download your Operator’s Manual",
       button: [
         {
-          href: "https://www.troybilt.com/en_US/operatorsmanuals",
+          href: "https://www.mtdparts.com/en_US/operatorsmanuals",
           copy: "Find manuals",
         },
       ],
@@ -62,7 +104,7 @@ const popularHelpLinks = {
       copy: "Find a licensed mechanic at a service center near you ",
       button: [
         {
-          href: "https://www.troybilt.com/on/demandware.store/Sites-troybilt-Site/en_US/Stores-FindService",
+          href: "https://www.mtdparts.com/en_US/servicelocator?showMap=true&horizontalView=true&isForm=true",
           copy: "Find service",
         },
       ],
@@ -72,7 +114,7 @@ const popularHelpLinks = {
       copy: "Find warranty information and extended plans",
       button: [
         {
-          href: "https://www.troybilt.com/en_US/warranty-faqs.html",
+          href: "https://www.mtdparts.com/en_US/warranty-details.html",
           copy: "Warranty FAQs",
         },
         // {
@@ -92,52 +134,28 @@ const findSupport = {
   h2: "Find Support by Model",
   blocks: [
     {
-      img: "https://www.troybilt.com/dw/image/v2/BCSH_PRD/on/demandware.static/-/Sites-mtd-master-catalog/default/dw26941dc0/products/Equipment/Troy-Bilt_Bronco46K_2000x2000_1.jpg",
+      img: "https://www.mtdparts.com/on/demandware.static/-/Sites-mtdparts-Library/default/dwa7a878a4/knowledge-center/Walk-Behind-Lawn-Mower-Blade-Replacement-Instructions.png",
       id: "Bilt_Bronco46K_2000x2000_1",
-      title: "Lawn Mowers",
-      url: "https://www.troybilt.com/en_US/riding-mower/",
+      title: "Maintenance",
+      url: "https://www.mtdparts.com/en_US/lawn-mower-snow-blower-maintenance/maintenance.html",
     },
     {
-      img: "https://www.troybilt.com/dw/image/v2/BCSH_PRD/on/demandware.static/-/Sites-mtd-master-catalog/default/dwc4e99be2/products/Equipment/Troy-Bilt_MustangZ54_2000x2000_6.jpg",
+      img: "https://www.mtdparts.com/on/demandware.static/-/Sites-mtdparts-Library/default/dwee8d1fe0/knowledge-center/images-mtdparts-com/repairs/Change-Mower-Wheel-Replace-a-Lawn-Mower-Wheel-with-Help-from-MTD.png",
       id: "Bilt_MustangZ54_2000x2000_6",
-      title: "Zero-Turn",
-      url: "https://www.troybilt.com/en_US/riding-lawn-mowers/zero-turn-mowers",
+      title: "Repairs",
+      url: "https://www.mtdparts.com/en_US/repairs.html",
     },
     {
-      img: "https://www.troybilt.com/dw/image/v2/BCSH_PRD/on/demandware.static/-/Sites-mtd-master-catalog/default/dw93aa9e08/products/Equipment/Troy-Bilt_TB230BXP_2000x2000_6.jpg",
+      img: "https://www.mtdparts.com/on/demandware.static/-/Sites-mtdparts-Library/default/dwe29b07d0/knowledge-center/images-mtdparts-com/Safety/Wood-Chipper-Safety-Get-Chipper-Shredder-Safety-Tips.png",
       id: "Bilt_TB230BXP_2000x2000_6",
-      title: "Walk-Behind Mowers",
-      url: "https://www.troybilt.com/en_US/walk-behind-mower/",
+      title: "Safety",
+      url: "https://www.mtdparts.com/en_US/safety.html",
     },
     {
-      img: "https://www.troybilt.com/dw/image/v2/BCSH_PRD/on/demandware.static/-/Sites-mtd-master-catalog/default/dw853c35dc/products/Equipment/Troy-Bilt_SuperBroncoCRT_2000x2000_10.jpg",
+      img: "https://www.mtdparts.com/on/demandware.static/-/Sites-mtdparts-Library/default/dw0fe950b5/knowledge-center/images-mtdparts-com/Safety/Self-Propelled-Mower-Safety-Lawn-Mower-Tips.png",
       id: "Cadet_ProZ_972SD_1",
-      title: "Tillers",
-      url: "https://www.troybilt.com/en_US/tillers/",
-    },
-    {
-      img: "https://www.troybilt.com/dw/image/v2/BCSH_PRD/on/demandware.static/-/Sites-mtd-master-catalog/default/dwe732a9dd/products/Equipment/Troy-Bilt_CSV070B_3.jpg",
-      id: "MX550",
-      title: "Chipper Shredders",
-      url: "https://www.troybilt.com/en_US/chipper-shredders/",
-    },
-    {
-      img: "https://www.troybilt.com/dw/image/v2/BCSH_PRD/on/demandware.static/-/Sites-mtd-master-catalog/default/dw87e9edff/products/Equipment/Troy-Bilt_TB51BP_2000x2000_1.jpg",
-      title: "Leaf Blowers",
-      id: "Cadet_3X_30HD_3",
-      url: "https://www.troybilt.com/en_US/leaf-blowers/",
-    },
-    {
-      img: "https://www.troybilt.com/dw/image/v2/BCSH_PRD/on/demandware.static/-/Sites-mtd-master-catalog/default/dwad09d29f/products/Equipment/Troy-Bilt_Vortex%202610_13.jpg",
-      id: "Cub_Cadet_CS16E_2000x2000_4",
-      title: "Snow",
-      url: "https://www.troybilt.com/en_US/snow-how-to/",
-    },
-    {
-      img: "https://www.troybilt.com/dw/image/v2/BCSH_PRD/on/demandware.static/-/Sites-mtd-master-catalog/default/dwede3d406/products/Attachments_Accessories/19A30031OEM_1.jpg",
-      id: "CC19A30018100",
-      title: "Attachments",
-      url: "https://www.troybilt.com/en_US/service-and-parts#attachments",
+      title: "General Advice & Tips",
+      url: "https://www.mtdparts.com/en_US/general-advice.html",
     },
   ],
 };
@@ -151,8 +169,8 @@ export default function CustomerService({}) {
             Help & Support
           </h1>
           <p className="mt-2 mb-0 text-center text-white max-w-3xl mx-auto">
-            We provide value to our brand owners with support content to help
-            you feel confident to tackle simple tasks or advice to extend the
+            We provide value to our customers with support content to help you
+            feel confident to tackle simple tasks or advice to extend the
             lifetime of your machine
           </p>
         </div>
@@ -236,7 +254,7 @@ export default function CustomerService({}) {
                   replacement parts, product manuals, or locating parts on your
                   machine.
                 </p>
-                <label htmlFor="search-by-number">
+                {/* <label htmlFor="search-by-number">
                   Model #, Part # or Item #
                 </label>
                 <input
@@ -246,7 +264,7 @@ export default function CustomerService({}) {
                   name="q"
                   type="search"
                   aria-describedby="16680858695420"
-                />
+                /> */}
                 <button
                   className="underline my-1 text-sm"
                   onClick={(e) => {
@@ -291,17 +309,17 @@ export default function CustomerService({}) {
             Don&apos;t have an account?
           </h2>
           <div className="text-center lg:px-32">
-            Find information related to your mower with ease when you{" "}
+            Find information related to your mower with ease when you
             <a
-              href="https://www.troybilt.com/en_US/login"
+              href="https://www.mtdparts.com/en_US/login"
               style={{ color: "#fff" }}
               className="font-bold"
             >
               create an account
-            </a>{" "}
-            and{" "}
+            </a>
+            and
             <a
-              href="https://www.troybilt.com/en_US/registration.html"
+              href="https://www.mtdparts.com/en_US/registration.html"
               style={{ color: "#fff" }}
               className="font-bold"
             >
@@ -361,8 +379,8 @@ export default function CustomerService({}) {
                     href={url}
                     className="flex flex-col no-underline items-center p-2 bg-neutral-100"
                   >
-                    <h3 className="">{title}</h3>
-                    <div className="bg-white p-1">
+                    <h3 className="text-xl">{title}</h3>
+                    <div className="bg-white">
                       <ResponsiveImage
                         id={id}
                         src={img}
@@ -393,7 +411,7 @@ export default function CustomerService({}) {
             <li className="d-flex flex-column quote col-12 col-sm-12 col-md-6 p-1">
               <div className="d-flex flex-column p-0">
                 <a
-                  href="tel:800-828-5500"
+                  href="tel:855-971-2271"
                   className="d-flex justify-content-center align-items-center py-2 px-5 border-0 no-underline"
                   style={{
                     background: "rgb(0, 0, 0)",
@@ -410,7 +428,7 @@ export default function CustomerService({}) {
                   >
                     <IconPhone className="fill-white" />
                   </span>
-                  800-828-5500
+                  855-971-2271
                 </a>
               </div>
             </li>
