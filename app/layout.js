7443cd11ed8b4386ec4f6b7dae88568cd32947ca
Todd Heckeler <todd@consumedesign.com>
2024-03-28 15:35:57 -0400

    updates

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
-// import "@/scss/main.scss";
 import "@/styles/globals.css";
+
 import Footer from "@/templates/Footer";
 import Header from "@/templates/Header";
 
@@ -11,23 +11,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Header />
-          <link
-            rel="stylesheet"
-            href="https://staging.mtdparts.com/on/demandware.static/Sites-mtdparts-Site/-/en_US/v1695748141977/css/global.css"
-          />
-
-          <link
-            rel="stylesheet"
-            href="https://www.mtdparts.com/on/demandware.static/Sites-mtdparts-Site/-/en_US/v1692119154763/css/product/detail.css"
-          />
-        </header>
-        <div style={{ minHeight: "200px" }}>{children}</div>
-        <footer>
-          <Footer />
-        </footer>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://staging.mtdparts.com/on/demandware.static/Sites-mtdparts-Site/-/en_US/v1695748141977/css/global.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://www.mtdparts.com/on/demandware.static/Sites-mtdparts-Site/-/en_US/v1692119154763/css/product/detail.css"
+        />
+      </head>
+      <body suppressHydrationWarning={true}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
